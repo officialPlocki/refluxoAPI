e39f3443ef9b06d2946436ea8ffb2105981cb2a5
EinfachKonrad <72760187+EinfachKonrad@users.noreply.github.com>
2021-08-05 21:48:50 +0200

    COMMIT

--- refluxoAPI/src/main/java/me/refluxo/refluxoapi/Refluxoapi.java
+++ refluxoAPI/src/main/java/me/refluxo/refluxoapi/Refluxoapi.java
@@ -6,7 +6,11 @@ public final class Refluxoapi extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        System.out.println("API wird geladen!");
+
+
+        System.out.println("API erfolgrich geladen");
 
     }
 

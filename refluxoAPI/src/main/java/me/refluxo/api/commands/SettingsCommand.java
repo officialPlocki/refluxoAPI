3264b54c239c28a7ac7bf60e6277162373dad25c
EinfachKonrad <72760187+EinfachKonrad@users.noreply.github.com>
2021-08-19 18:18:42 +0200

    EinfachKonrad per IntelliJ Idea Community Edition

--- refluxoAPI/src/main/java/me/refluxo/api/commands/SettingsCommand.java
+++ refluxoAPI/src/main/java/me/refluxo/api/commands/SettingsCommand.java
@@ -40,7 +40,7 @@ public class SettingsCommand extends Language implements CommandExecutor, Listen
             case EVERY:
                 ChatADs = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
                 ChatADsName = "Alle ChatAds";
-                ChatADsDes = "§e§lAlle ChatADs\n§2Nur ChatADs vom Server\nNur ChatADs von Spielern\nkeine ChatADs"
+                ChatADsDes = "\n§e§lAlle ChatADs\n§2Nur ChatADs vom Server\nNur ChatADs von Spielern\nkeine ChatADs\n§bzum ändern klicken!"
                 break;
             case SERVER:
                 ChatADs = new ItemStack(Material.ORANGE_STAINED_GLASS_PANE);
